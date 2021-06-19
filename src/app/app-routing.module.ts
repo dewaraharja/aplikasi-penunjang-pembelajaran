ab5e93e2fe6bfd00facbab57dcd014057df866a9
DEWA RAHARJA ADJISAKA <dewa1900016053@webmail.uad.ac.id>
2021-06-19 23:04:46 +0700

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './aut/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:'login',
+  component:LoginComponent
+},
+{
+ path:'register',
+ component:RegisterComponent 
+},
+{
+  path:'',
+  pathMatch:'full',
+  redirectTo:'/login'
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
